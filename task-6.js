8242f151f722a3e5062bb8bafce7a51f866aca5b
Sergey <shapa92sergey@gmail.com>
2020-05-01 17:39:37 +0300

    Finish 6-task

---
+++ task-6.js
@@ -0,0 +1,30 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = function(allProdcuts, productName) {
+  let obj;
+  let elementObj;
+  let totalPrice;
+  for (let i = 0; i < allProdcuts.length; i += 1) {
+    obj = allProdcuts[i];
+    // console.log(obj);
+    // eslint-disable-next-line
+    for (elementObj in obj) {
+      elementObj = Object.values(obj);
+      //   console.log(elementObj);
+      if (elementObj.includes(productName)) {
+        totalPrice = obj.price * obj.quantity;
+      }
+    }
+  }
+
+  return totalPrice;
+};
+
+console.log(calculateTotalPrice(products, 'Радар')); // 5200
+
+console.log(calculateTotalPrice(products, 'Дроид')); // 2800
